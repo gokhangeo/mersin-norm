d85482979b87784c9a9046a3bece468c18ae5c91
gelgul <gokhantkgm44@gmail.com>
2025-11-30 17:45:22 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+// service-worker.js
+const CACHE_NAME = 'norm-v1';
+const urlsToCache = ['./', './index.html'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
